ab6e7bd3800fa4cafd8955f3fd8d0c419d8bb097
ahmadguze <61331921+ahmadguze@users.noreply.github.com>
2021-07-25 16:58:17 +0300

    Make another route for the same logic

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import Flask, request
 app = Flask(__name__)
 
 
+@app.get('/oOfc')
 @app.get('/occurrencesOfCharacters')
 def occurrences_of_characters():
     str = request.args.get('str')
